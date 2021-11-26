e5af973243934504ba40f67b46d33722a9eef5d2
jravaell <jravaell@gmail.com>
2021-11-26 18:36:57 +0700

    Update

--- src/components/ChannelContainer.jsx
+++ src/components/ChannelContainer.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import { Channel, useChatContext, MessageTeam } from 'stream-chat-react';
 import {ChannelInner, CreateChannel, EditChannel} from './';
--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, {useState, useEffect} from "react";
 import { Avatar, useChatContext } from 'stream-chat-react';
 
