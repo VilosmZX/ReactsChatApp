cd897e746d6302eb8e47a6afae67b15199617960
jravaell <jravaell@gmail.com>
2021-11-26 17:45:29 +0700

    a

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -29,7 +29,7 @@ const Auth = () => {
         e.preventDefault();
         const { username, password, phoneNumber, avatarURL } = form;
 
-        const URL = 'http://localhost:5000/auth';   
+        const URL = 'https://jsocialweb.herokuapp.com/auth';   
 
         const {data: {token, userId, hashedPassword, fullName}} = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
             username, password, fullName: form.fullName, phoneNumber, avatarURL
